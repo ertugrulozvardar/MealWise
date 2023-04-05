3d42b1fb9081b46fdac7ed68889bbe866dd015ec
ertugrulozvardar <ozvardarertugrul@gmail.com>
2023-04-05 14:08:13 +0300

    Initial Commit

---
+++ MealWise/MealWiseApp.swift
@@ -0,0 +1,17 @@
+//
+//  MealWiseApp.swift
+//  MealWise
+//
+//  Created by ertugrul.ozvardar on 5.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct MealWiseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
